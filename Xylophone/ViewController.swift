a01e0216a6cecf28eb7a3a4c508b41688a731c9a
Jomar Leano <jomar35leano@gmail.com>
2022-09-04 16:39:28 +0800

    done with the animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!);
+        sender.alpha = 0.5;
+        print("start")
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.2){
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1;
+                print("end")
+            }
+        }
     }
     
     func playSound(soundName : String) {
