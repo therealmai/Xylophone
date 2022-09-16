8cff984ab431c5854425506a7c2989fa1dcab62a
Jomar Leano <jomar35leano@gmail.com>
2022-09-16 22:01:17 +0800

    Another commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -34,8 +34,9 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
+    
+    //test
     
 }
 
